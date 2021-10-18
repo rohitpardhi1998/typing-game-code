f5a7cb01b9f59a17f0509af91d9eae7ee9090938
Rohit Pardhi <rohit@kd-918a004c-2910-11b2-a85c-9b3218f1416d.ibm.com>
2021-10-18 22:45:33 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect ,useRef} from "react"
+
 
 function App() {
+const STARTING_TIME = 60
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+    const textBoxRef = useRef(null)
+    
+
+  function handleChange(event) {
+    const { value } = event.target
+    setText(value)
+  }
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ")
+    console.log(wordsArr.length)
+    return wordsArr.filter(word => word !== "").length
+  }
+
+
+  function startGame() {
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText("")
+     textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(time => time - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      endGame()
+
+    }
+  }, [timeRemaining, isTimeRunning])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Speed Typing Game</h1>
+      <textarea
+         ref={textBoxRef}
+        onChange={handleChange}
+         placeholder="Test Your Typing Speed Here"
+
+        value={text}
+        disabled={!isTimeRunning}
+      />
+
+      <h4> Time Remaining;  {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning} >Start Game</button>
+      <h1>Word Count is:{wordCount}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
